a651a9a725d22498865cca77755c54047956c7ee
ImAkhil24 <akhilbadoni24@gmail.com>
2026-05-20 02:05:49 +0530

    version 2

--- src/RateLimiter.java
+++ src/RateLimiter.java
@@ -1,6 +1,8 @@
 import java.time.Duration;
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 
@@ -8,6 +10,9 @@ import java.util.Queue;
 // when new otp request comes, so check whether in the time window of current time - 5 minutes
 // do we have more than 3 otp request
 // if we do then reject else accept and add
+
+// for V2 I can just make different queues based on the hash for each user, but it's bad
+// just maintain the already existing count on the hash
 
 class Request {
     String userId;
@@ -22,10 +27,12 @@ class Request {
 public class RateLimiter {
     // maintain the 5 minutes time frame
     Queue<Request> requestQueue= new LinkedList<>();
+    Map<String, Integer> globalCount = new HashMap<String, Integer>();
 
     void purgeRequests(Instant now) {
         while(!requestQueue.isEmpty()) {
             if(Duration.between(requestQueue.peek().time, now).toMinutes() >= 5) {
+                globalCount.put(requestQueue.peek().userId,globalCount.get(requestQueue.peek().userId) - 1);
                 requestQueue.poll();
             } else {
                 break;
@@ -37,13 +44,12 @@ public class RateLimiter {
         Instant now = Instant.now();
         System.out.println(now);
         purgeRequests(now);
-        int counter = 0;
-        for(Request req: requestQueue) {
-            if(req.userId.equals(userId)) counter++;
-        }
+        Integer counter = globalCount.getOrDefault(userId, 0);
+
         System.out.println(counter);
         if(counter >= 3) return false;
         requestQueue.add(new Request(userId, now));
+        globalCount.put(userId, globalCount.getOrDefault(userId, 0) + 1);
         return true;
     }
 
